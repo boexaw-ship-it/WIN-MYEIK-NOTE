a35328802c7b5c7b249b7cf179a77ecff0cf6250
zawzaw <boexaw@gmail.com>
2026-06-07 12:45:54 +0630

    Update delivery unit to use 'Time' instead of currency

--- assets/js/config.js
+++ assets/js/config.js
@@ -32,7 +32,7 @@ const BUY_ITEMS = [
     { name: "ရေခူ",               units: ["ပိဿာ", "ကျပ်သား"] },
     { name: "တန်ဆာခ (Myein)",      units: ["ပိဿာ", "ကျပ်သား"] },
     { name: "တန်ဆာခ (Taxi)",       units: ["ပိဿာ", "ကျပ်သား"] },
-    { name: "တန်ဆာခ (Delivery)",   units: ["ပိဿာ", "ကျပ်သား"] },
+    { name: "တန်ဆာခ (Delivery)",   units:["Time"] },
     { name: "ပလတ်စတစ်အိတ်",     units: ["ပါကင်"] },
     { name: "ပလတ်စတစ်ဗူး",      units: ["ဗူး"] }
 ];
