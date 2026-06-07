eacd4afdaf4dd5f8578481cf5e9e3e7d8bf3cfb4
zawzaw <boexaw@gmail.com>
2026-06-07 13:16:20 +0630

    Update units for Taxi and Delivery in config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -31,8 +31,8 @@ const BUY_ITEMS = [
     { name: "ဒူးရင်းယို",        units: ["ပိဿာ", "ကျပ်သား"] },
     { name: "ရေခူ",               units: ["ပိဿာ", "ကျပ်သား"] },
     { name: "တန်ဆာခ (Myein)",      units: ["ပိဿာ", "ကျပ်သား"] },
-    { name: "တန်ဆာခ (Taxi)",       units: ["ပိဿာ", "ကျပ်သား"] },
-    { name: "တန်ဆာခ (Delivery)",   units:["Time"] },
+    { name: "တန်ဆာခ (Taxi)",       units: ["Time"] } },
+    { name: "တန်ဆာခ (Delivery)",   units: ["Time"] },
     { name: "ပလတ်စတစ်အိတ်",     units: ["ပါကင်"] },
     { name: "ပလတ်စတစ်ဗူး",      units: ["ဗူး"] }
 ];
